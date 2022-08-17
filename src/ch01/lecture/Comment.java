714f7d65fb6109094e86ea7dbcb55ddec3228369
Kimchi.Disaster <ha971206@gmail.com>
2022-08-17 12:13:56 +0900

    Initial commit

---
+++ src/ch01/lecture/Comment.java
@@ -0,0 +1,25 @@
+package ch01.lecture;
+
+public class Comment {
+	public static void main(String[] args) {
+//		주석 : 프로그램 실행에 영향을 미치지 않음
+		// 한줄 주석
+		/*
+		 긴
+		 줄
+		 주
+		 석
+		 */
+		
+		// 단축키 : 컨슬래시
+		
+		// 힌트 얻기 : 컨스페
+		// 한줄 지우기 : 컨D
+		// 코드 위아래로 옮기기 : 알트 상하
+
+		// 콘솔 출력 코드 : sysout / sout
+		System.out.println("Hello World!");
+		System.out.println("자바는 쉽지않다");
+		System.out.println("sysout 컨스페 쓰면 편함");
+	}
+}
